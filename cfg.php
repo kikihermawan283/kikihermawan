b7c2966f091952c26a2f66ffb39316d87e099f15
kikihermawan283 <75632944+kikihermawan283@users.noreply.github.com>
2020-12-08 00:17:09 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,11 @@
+<?php
+// masukan data user
+
+
+$data = '{"targetId":"1328988","userId":"1328988","sessionToken":"1BYdAXkei9cJwad","virtualDeviceId":"9f4be1932158ad9cff6752a552ee9751","timestamp":1604062892908,"version":"2.0.8","sign":"436829b0995ebf86e4ff33e209d1e9a3"}';
+
+// masukan data headers
+
+$useragent = 'Dart/2.8 (dart:io)';
+$contenttype = 'application/json; charset=utf-8';
+$host = 'app.v-tube.biz';
